886ff72979d32c0adc030ce284f0033f8a0161f5
lunazivkovic-giro <luna.zivkovic@gogiro.app>
2024-07-17 11:09:25 +0200

    fixes

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -8,7 +8,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { of } from 'rxjs';
 
 class MockAuthService {
   login(email: string, password: string): boolean {
